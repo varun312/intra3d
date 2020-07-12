13f81db369c0f70646b2c6dc32608f674038eeb2
varun312 <32396077+varun312@users.noreply.github.com>
2020-07-12 14:51:02 +0530

    Files to PC from laptop

---
+++ ChangeHEight.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeHEight : MonoBehaviour
+{
+    public Terrain terrain;
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(30, 30, 200, 30), "Change height")) 
+        {
+            int xres = terrain.terrainData.heightmapResolution;
+            int yres = terrain.terrainData.heightmapResolution;
+
+            float[,] heights = terrain.terrainData.GetHeights(0, 0, xres, yres);
+
+
+            for (int i = 0; i < 513; i++)
+            {
+                for (int x = 0; x < 513; x++)
+                {
+                    heights[i, x] = 0;
+                }
+            }
+
+            //heights[10, 10] = 1f;
+            terrain.terrainData.SetHeights(0,0,heights);
+
+        }
+
+
+    }
+}
